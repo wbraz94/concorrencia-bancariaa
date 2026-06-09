1d3bcd9226ba33578bd85d7e6dddb71d09af7f88
wbraz94 <welersonbrs@gmail.com>
2026-06-09 06:38:49 -0300

    Delete concorrencia-bancaria/concorrencia-bancaria/src/main directory

--- concorrencia-bancaria/concorrencia-bancaria/src/main/java/com/example/bancaria/repository/ContaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bancaria.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.example.bancaria.entity.ContaBancaria;
-
-@Repository
-public interface ContaRepository extends JpaRepository<ContaBancaria, Long> {
-}
--- concorrencia-bancaria/concorrencia-bancaria/src/main/java/com/example/bancaria/repository/ContaVersionadaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.bancaria.repository;
-
-import com.example.bancaria.entity.ContaBancariaVersionada;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContaVersionadaRepository extends JpaRepository<ContaBancariaVersionada, Long> {
-}
